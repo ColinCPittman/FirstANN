275be291e088d3c1f48e7c823c7b6e89e8c24555
ColinCPittman <colincpittman@gmail.com>
2024-12-27 11:06:12 -0500

    implemented forward def

--- neural_net.py
+++ neural_net.py
@@ -9,7 +9,24 @@ class NeuralNetwork:
        self.bias_output = np.random.randn(output_size)
 
     def forward(self, inputs):
-        pass
+        # Calculate the weighted input for the hidden layer
+        z_hidden = np.dot(inputs, self.weights_input_hidden) + self.bias_hidden
+
+        # Apply the activation function
+        a_hidden = self.relu(z_hidden)
+
+        # Calculate the weighted input for the output layer
+        z_output = np.dot(a_hidden, self.weights_hidden_output) + self.bias_output
+
+        a_output = self.softmax(z_output)
+        
+        return a_output
+    def softmax(self, x):
+        exp_x = np.exp(x-np.max(x, axis=1, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+
+    def relu(self, z_hidden):
+        return np.maximum(0, z_hidden)
 
     def backward(self, gradients):
         pass
@@ -35,3 +52,7 @@ if __name__ == "__main__":
     print("\nWeights from Hidden to Output Layer:\n", nn.weights_hidden_output)
     print("\nBias for Hidden Layer:\n", nn.bias_hidden)
     print("\nBias for Output Layer:\n", nn.bias_output)
+
+    # Perfom forward pass
+    output = nn.forward(inputs)
+    print("\nOutput from the forward pass:\n", output)
